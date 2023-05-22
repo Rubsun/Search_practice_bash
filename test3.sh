3b21d5ad44d5aeb177cbaf5f3d3a5b3ce525f11c
Maksim <74344870+Rubsun@users.noreply.github.com>
2023-05-22 22:03:09 +0300

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -3,11 +3,12 @@
 chmod +x search_word.sh
 echo "Запушен тест на проверку корректную работу"
 
-./search_word.sh input output
+./search_word.sh Оля <<< "input_file output_file"
 
 if [[ $? -eq 0 ]]
 then
     echo "Тест выполнен успешно, скрипт работает исправно"
+    exit 0
 else
     echo "Тест провален, скрипт работает некорректно"
     exit 1
